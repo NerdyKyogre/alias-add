28532112fb25c93feef62499e60614a52f00cf8e
Cole Franchuk <cfranchuk@vivaldi.net>
2019-12-13 10:00:53 -0700

    Update alias-add.py

--- alias-add.py
+++ alias-add.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Wed Dec  11 12:31:31 2019
@@ -6,23 +5,31 @@ Created on Wed Dec  11 12:31:31 2019
 @author: NerdyKyogre
 """
 """
-alias-add v0.0.2
+alias-add v0.0.3
 This program appends aliases permanently to shell rc files of any user for those not comfortable hand editing config files. Supports bash and zsh on MacOS, Linux, and BSD.
 """
+
 import os
 
 def start():
     print("Welcome to alias-add. This program will permanently add your shell aliases so you don't have to.\nFirst, let's determine what sort of system you have.")
     sh = input("Are you using bash or zsh?\nIf you don't know and you are on a mac with MacOS 10.15 Catalina or later, you probably have zsh. Otherwise, you probably have bash.\nIf you are using bash, type b or bash. For zsh, type z or zsh.\n")
+    while sh != "b"and sh != "bash" and sh != "z" and sh != "zsh":
+        sh = input("That's not a valid shell. Are you using bash or zsh? ")
     user = input("What user would you like to add the alias(es) to? Note: In order to add to root, this program must be run as root.\n")
     if user == "root":
         rootlink = os.path.exists("/home/root")
         if rootlink == False:
             os.symlink("/root", "/home/root")
+        else:
+            linkexists = input("Error: link already exists at /home/root. Continue? Y/N: ")
+            if linkexists != "Y" and linkexists != "y":
+                print("Goodbye, and thank you for using alias-add.")
+                exit()
     if sh == "b" or sh == "bash":
         bash(user)
     if sh == "z" or sh == "zsh":
-        zsh(user)    
+        zsh(user)   
     if user == "root":
         if rootlink == False:
             os.unlink("/home/root")
@@ -44,8 +51,11 @@ def bash(user):
         if extLine == "N" or extLine == "n":
             break
         with open("/home/"+user+"/.bashrc", "a") as shrc:
-            append = input("Put your extra lines below, separated by '\\n':\n")
-            shrc.write("\n"+append+"\n")
+            append = "null"
+            while append != "Finish":
+                append = input("Please input another line, or type 'Finish' to end the program: ")
+                if append != "Finish":
+                    shrc.write("\n"+append)
             break
 
 def zsh(user):
@@ -63,9 +73,12 @@ def zsh(user):
         extLine = input ("Would you like to add any more lines to your shell rc file? Y/N: ")
         if extLine == "N" or extLine == "n":
             break
-        with open("/home/"+user+"/.bashrc", "a") as shrc:
-            append = input("Put your extra lines below, separated by '\\n':\n")
-            shrc.write("\n"+append+"\n")
+        with open("/home/"+user+"/.zshrc", "a") as shrc:
+            append = "null"
+            while append != "Finish":
+                append = input("Please input another line, or type 'Finish' to end the program: ")
+                if append != "Finish":
+                    shrc.write("\n"+append)
             break
     
 start()
